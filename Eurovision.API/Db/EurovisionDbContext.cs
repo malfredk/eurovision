11f91e42b97d7547ca64531473e5b3f2565a79e0
Malene Fredheim <malene.fredheim@skrivebord.onmicrosoft.com>
2023-10-04 12:50:45 +0200

    renaming context

---
+++ Eurovision.API/Db/EurovisionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Eurovision.Entity;
+
+namespace Eurovision.API.Data;
+
+public class EurovisionDbContext : DbContext
+{
+    public EurovisionDbContext (DbContextOptions<EurovisionDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<PlayerEntity> Players { get; set; } = default!;
+}
